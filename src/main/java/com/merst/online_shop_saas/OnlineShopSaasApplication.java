21ba7908a043f6d43e2c44136264e5f36420bd2b
Merst <noxlittner@gmail.com>
2024-09-08 10:04:15 -0400

    Spring boot project initialization

---
+++ src/main/java/com/merst/online_shop_saas/OnlineShopSaasApplication.java
@@ -0,0 +1,13 @@
+package com.merst.online_shop_saas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopSaasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShopSaasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/merst/online_shop_saas/OnlineShopSaasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.merst.online_shop_saas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShopSaasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
