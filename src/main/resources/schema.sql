76e7ee89da8cfd93736fef9ce33276b408a66c82
Merst <noxlittner@gmail.com>
2024-09-13 12:24:16 -0400

    SQL Schema debugging and completion

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,21 +1,15 @@
-DROP TABLE IF EXISTS "Customer";
-DROP TABLE IF EXISTS "Product";
-DROP TABLE IF EXISTS "Address";
-DROP TABLE IF EXISTS "Category";
-DROP TABLE IF EXISTS "Supplier";
-DROP TABLE IF EXISTS "Cart";
-DROP TABLE IF EXISTS "Cart_item";
-DROP TABLE IF EXISTS "Order";
-DROP TABLE IF EXISTS "Order_item";
-DROP TABLE IF EXISTS "Payment";
-DROP TABLE IF EXISTS "Review";
 
+DROP TYPE IF EXISTS customer_locale CASCADE;
 CREATE TYPE customer_locale AS ENUM ('EN', 'FR');
+
+DROP TYPE IF EXISTS payment_method CASCADE;
 CREATE TYPE payment_method AS ENUM ('Credit', 'Venmo', 'Paypal', 'Other'); --Find more methods
+
+DROP TYPE IF EXISTS payment_status CASCADE;
 CREATE TYPE payment_status AS ENUM ('Complete', 'Pending', 'Incomplete', 'Refunded'); 
 
-CREATE TABLE "Customer" (
-    "id" uuid DEFAULT gen_random_uuid(),
+CREATE TABLE IF NOT EXISTS "customer" (
+    "id" uuid NOT NULL ,
     "first_name" text NOT NULL,
     "middle_name" text NOT NULL,
     "last_name" text NOT NULL,
@@ -24,11 +18,25 @@ CREATE TABLE "Customer" (
     "created_at" timestamp NOT NULL,
     "last_login" timestamp NOT NULL,
     "email_validated" boolean DEFAULT FALSE,
-    CONSTRAINT "customer.pkey" PRIMARY KEY ("id")
+    PRIMARY KEY ("id")
 );
 
-CREATE TABLE "Product" (
-    "id" uuid DEFAULT gen_random_uuid(),
+CREATE TABLE IF NOT EXISTS "supplier" (
+    "id" uuid NOT NULL,
+    "name" text NOT NULL,
+    PRIMARY KEY ("id")
+);
+
+CREATE TABLE IF NOT EXISTS "category" (
+    "id" uuid NOT NULL,
+    "parent_category_id" uuid,
+    "description" text,
+    PRIMARY KEY ("id"),
+    FOREIGN KEY (parent_category_id) REFERENCES category(id)
+);
+
+CREATE TABLE IF NOT EXISTS "product" (
+    "id" uuid NOT NULL,
     "supplier_id" uuid NOT NULL,
     "category_id" uuid NOT NULL,
     "name" text NOT NULL,
@@ -36,16 +44,16 @@ CREATE TABLE "Product" (
     "is_available" boolean NOT NULL,
     "sale_multiplier" numeric(3,2) DEFAULT 1.00,
     "description" text,
-    CONSTRAINT "product.pkey" PRIMARY KEY ("id"),
-    CONSTRAINT "fk_supplier" FOREIGN KEY (supplier_id) REFERENCES supplier(id),
-    CONSTRAINT "fk_category" FOREIGN KEY (category_id) REFERENCES category(id)
+    PRIMARY KEY ("id"),
+    FOREIGN KEY (supplier_id) REFERENCES supplier(id),
+    FOREIGN KEY (category_id) REFERENCES category(id)
 );
 
-CREATE TABLE "Address" (
-    "id" uuid DEFAULT gen_random_uuid(),
+CREATE TABLE IF NOT EXISTS "address" (
+    "id" uuid NOT NULL,
     "customer_id" uuid NOT NULL,
     "digits" integer NOT NULL,
-    "street", text NOT NULL,
+    "street" text NOT NULL,
     -- TODO: City enum
     "city" text NOT NULL,
     -- TODO: Country enum
@@ -54,73 +62,61 @@ CREATE TABLE "Address" (
     "state_province" text NOT NULL,
     "zipcode" text NOT NULL,
     "nickname" text,
-    CONSTRAINT "address.pkey" PRIMARY KEY ("id"),
-    CONSTRAINT "fk_customer" FOREIGN KEY (customer_id) REFERENCES customer(id)
+    PRIMARY KEY ("id"),
+    FOREIGN KEY (customer_id) REFERENCES customer(id)
 );
 
-CREATE TABLE "Category" (
-    "id" uuid DEFAULT gen_random_uuid(),
-    "parent_category_id" uuid,
-    "description" text,
-    CONSTRAINT "category.pkey" PRIMARY KEY ("id"),
-    CONSTRAINT "fk_parent_category" FOREIGN KEY (parent_category_id) REFERENCES category(id)
-);
-
-CREATE TABLE "Supplier" (
-    "id" uuid DEFAULT gen_random_uuid(),
-    "name" text NOT NULL,
-    CONSTRAINT "supplier.pkey" PRIMARY KEY ("id")
-);
-
-CREATE TABLE "Cart" (
-    "id" uuid DEFAULT gen_random_uuid(),
+CREATE TABLE IF NOT EXISTS "cart" (
+    "id" uuid NOT NULL,
     "customer_id" uuid NOT NULL,
     "total" numeric(9,2) NOT NULL,
-    CONSTRAINT "cart.pkey" PRIMARY KEY ("id"),
-    CONSTRAINT "fk_customer" FOREIGN KEY (customer_id) REFERENCES customer(id)
+    PRIMARY KEY ("id"),
+    FOREIGN KEY (customer_id) REFERENCES customer(id)
 );
 
-CREATE TABLE "Cart_item" (
-    "id" uuid DEFAULT gen_random_uuid(),
+CREATE TABLE IF NOT EXISTS "cart_item" (
+    "id" uuid NOT NULL,
     "product_id" uuid NOT NULL, 
     "quantity" integer NOT NULL,
-    CONSTRAINT "cart_item.pkey" PRIMARY KEY ("id"),
-    CONSTRAINT "fk_product" FOREIGN KEY (product_id) REFERENCES product(id)
+    PRIMARY KEY ("id"),
+    FOREIGN KEY (product_id) REFERENCES product(id)
 );
 
-CREATE TABLE "Order" (
-    "id" uuid DEFAULT gen_random_uuid(),
+CREATE TABLE IF NOT EXISTS "invoice" (
+    "id" uuid NOT NULL,
     "customer_id" uuid NOT NULL,
-    CONSTRAINT "order.pkey" PRIMARY KEY ("id"),
-    CONSTRAINT "fk_customer" FOREIGN KEY (customer_id) REFERENCES customer(id)
+    PRIMARY KEY ("id"),
+    FOREIGN KEY (customer_id) REFERENCES customer(id)
 );
 
-CREATE TABLE "Order_item" (
-    "id" uuid DEFAULT gen_random_uuid(),
+CREATE TABLE IF NOT EXISTS "invoice_item" (
+    "id" uuid NOT NULL,
     "product_id" uuid NOT NULL,
-    "quantity" NOT NULL DEFAULT 1,
+    "invoice_id" uuid NOT NULL,
+    "quantity" integer DEFAULT 1,
     "subtotal" numeric(9,2) NOT NULL,
-    CONSTRAINT "order_item.pkey" PRIMARY KEY ("id"),
-    CONSTRAINT "fk_product" FOREIGN KEY (product_id) REFERENCES product(id)
+    PRIMARY KEY ("id"),
+    FOREIGN KEY (product_id) REFERENCES product(id),
+    FOREIGN KEY (invoice_id) REFERENCES invoice(id)
 );
 
-CREATE TABLE "Payment" (
-    "id" uuid DEFAULT gen_random_uuid(),
-    "order_id" uuid NOT NULL,
+CREATE TABLE IF NOT EXISTS "review" (
+    "id" uuid NOT NULL,
+    "customer_id" uuid NOT NULL,
+    "product_id" uuid NOT NULL,
+    "rating" integer NOT NULL,
+    PRIMARY KEY ("id"),
+    FOREIGN KEY (customer_id) REFERENCES customer(id),
+    FOREIGN KEY (product_id) REFERENCES product(id)
+);
+
+CREATE TABLE IF NOT EXISTS "payment" (
+    "id" uuid NOT NULL,
+    "invoice_id" uuid NOT NULL,
     "method" payment_method NOT NULL,
     "status" payment_status NOT NULL,
     "created_at" timestamp NOT NULL,
     "completed_at" timestamp NOT NULL,
-    CONSTRAINT "payment.pkey" PRIMARY KEY ("id"),
-    CONSTRAINT "fk_order" FOREIGN KEY (order_id) REFERENCES order(id)
-);
-
-CREATE TABLE "Review" (
-    "id" uuid DEFAULT gen_random_uuid(),
-    "customer_id" uuid NOT NULL,
-    "product_id" uuid NOT NULL,
-    "rating" integer NOT NULL,
-    CONSTRAINT "review.pkey" PRIMARY KEY ("id"),
-    CONSTRAINT "fk_customer" FOREIGN KEY (customer_id) REFERENCES customer(id),
-    CONSTRAINT "fk_product" FOREIGN KEY (product_id) REFERENCES product(id)
+    PRIMARY KEY ("id"),
+    FOREIGN KEY (invoice_id) REFERENCES invoice(id)
 );
